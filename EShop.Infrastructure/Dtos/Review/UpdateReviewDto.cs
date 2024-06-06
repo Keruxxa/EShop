858bd33ab1cf0e4dd36969e0743e9c82416ebb5b
Pashinin Kirill <k-pashinin@mail.ru>
2024-06-06 14:28:01 +1000

    Added UpdateReviewDto

---
+++ EShop.Infrastructure/Dtos/Review/UpdateReviewDto.cs
@@ -0,0 +1,7 @@
+﻿namespace EShop.Infrastructure.Dtos.Review
+{
+    /// <param name="ProductId"> Id товара, связанного с отзывом </param>
+    /// <param name="Rating"> Рейтинг </param>
+    /// <param name="Text"> Текстовая часть отзыва </param>
+    public record UpdateReviewDto(Guid ProductId, int Rating, string? Text);
+}
