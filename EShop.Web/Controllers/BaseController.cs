2c04d614d4c8c92bdf0e04a9242ed5669b88ef63
Кирилл Пашинин <k-pashinin@mail.ru>
2024-06-12 20:05:23 +1000

    Added BaseController and ProductController

---
+++ EShop.Web/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace EShop.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator;
+
+        internal Guid UserId => User.Identity.IsAuthenticated
+            ? Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)
+            : Guid.Empty;
+    }
+}
