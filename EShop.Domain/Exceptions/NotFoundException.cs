4bba1fc260a3bcdd4ecb83cb12148ff9318bf3a5
Кирилл Пашинин <k-pashinin@mail.ru>
2024-06-12 17:44:08 +1000

    Added a new base exception

---
+++ EShop.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace EShop.Domain.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string entityType, object id)
+            : base($"Entity '{entityType}' with id '{id}' not found")
+        {
+        }
+    }
+}
