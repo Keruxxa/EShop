135f3702185855478ecb72df27c4131cb7f8928d
Кирилл Пашинин <k-pashinin@mail.ru>
2024-07-31 11:46:55 +1000

    Add password hasher interface and realization

---
+++ EShop.Application/Interfaces/Security/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace EShop.Application.Interfaces.Security
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+
+        bool Verify(string password, string hashPassword);
+    }
+}
