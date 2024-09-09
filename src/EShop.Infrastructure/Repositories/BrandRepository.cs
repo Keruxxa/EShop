b43235e7d5401ed288383e3a01f15e2ae8f4b34f
Кирилл Пашинин <k-pashinin@mail.ru>
2024-09-09 14:58:11 +1000

    Add BrandRepository

---
+++ src/EShop.Infrastructure/Repositories/BrandRepository.cs
@@ -0,0 +1,51 @@
+﻿using EShop.Application.Interfaces;
+using EShop.Application.Interfaces.Repositories;
+using EShop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Infrastructure.Repositories;
+
+public class BrandRepository : IBrandRepository
+{
+    private readonly IEShopDbContext _dbContext;
+
+    public BrandRepository(IEShopDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+
+    public async Task<List<Brand>> GetList(CancellationToken cancellationToken)
+    {
+        return await _dbContext.Brands
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<Brand> GetById(int id, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Brands
+            .Include(brand => brand.BrandProducts)
+            .FirstOrDefaultAsync(brand => brand.Id == id, cancellationToken);
+    }
+
+    public int Create(Brand brand)
+    {
+        return _dbContext.Brands.Add(brand).Entity.Id;
+    }
+
+    public void Update(Brand brand)
+    {
+        _dbContext.Brands.Update(brand);
+    }
+
+    public void Delete(Brand brand)
+    {
+        _dbContext.Brands.Remove(brand);
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        return await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
