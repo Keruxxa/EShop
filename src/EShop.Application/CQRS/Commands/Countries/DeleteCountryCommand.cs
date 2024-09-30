d9f497245a438e952650821e52818b7880dace14
Pashinin Kirill <Kirill.Pashinin@icl-services.com>
2024-09-30 13:32:02 +0300

    Country Result return refactoring

--- src/EShop.Application/CQRS/Commands/Countries/DeleteCountryCommand.cs
+++ src/EShop.Application/CQRS/Commands/Countries/DeleteCountryCommand.cs
@@ -1,4 +1,5 @@
 ﻿using CSharpFunctionalExtensions;
+using EShop.Application.Issues.Errors.Base;
 using MediatR;
 
 namespace EShop.Application.CQRS.Commands.Countries;
@@ -6,6 +7,6 @@ namespace EShop.Application.CQRS.Commands.Countries;
 /// <summary>
 ///     Представляет команду для удаления страны
 /// </summary>
-public record DeleteCountryCommand(int Id) : IRequest<Result>
+public record DeleteCountryCommand(int Id) : IRequest<Result<Unit, Error>>
 {
 }
