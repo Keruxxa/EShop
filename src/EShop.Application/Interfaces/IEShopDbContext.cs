e45da90358815df7f575d13708f87a55d6233182
Pashinin Kirill <Kirill.Pashinin@icl-services.com>
2024-10-07 07:13:02 +0300

    DbContext review

--- src/EShop.Application/Interfaces/IEShopDbContext.cs
+++ src/EShop.Application/Interfaces/IEShopDbContext.cs
@@ -5,20 +5,21 @@ namespace EShop.Application.Interfaces;
 
 public interface IEShopDbContext
 {
-    public DbSet<Product> Products { get; set; }
-    public DbSet<Category> Categories { get; set; }
-    public DbSet<Favorite> Favorites { get; set; }
-    public DbSet<Brand> Brands { get; set; }
-    public DbSet<CategoryProducts> CategoryProducts { get; set; }
-    public DbSet<FavoriteProducts> FavoriteProducts { get; set; }
-    public DbSet<BrandProducts> BrandProducts { get; set; }
     public DbSet<Basket> Baskets { get; set; }
-    public DbSet<BasketItem> BasketsItems { get; set; }
-    public DbSet<Review> Reviews { get; set; }
+    public DbSet<BasketItem> BasketItems { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+    public DbSet<BrandProducts> BrandProducts { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<CategoryProducts> CategoryProducts { get; set; }
     public DbSet<Country> Countries { get; set; }
-    public DbSet<User> Users { get; set; }
-    public DbSet<Role> Roles { get; set; }
+    public DbSet<Favorite> Favorites { get; set; }
+    public DbSet<FavoriteProducts> FavoriteProducts { get; set; }
     public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<User> Users { get; set; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
