e4f3401b44297d4db00ff941e6af035ebaeb1ba5
Pashinin Kirill <Kirill.Pashinin@icl-services.com>
2024-10-13 16:02:11 +0300

    Add folders for client project

--- src/EShop.Web/ClientApp/src/app/app.module.ts
+++ src/EShop.Web/ClientApp/src/app/app.module.ts
@@ -1,31 +1,19 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PrimeNGConfig } from 'primeng/api';
-import { ButtonModule } from 'primeng/button';
-import { CheckboxModule } from 'primeng/checkbox';
-import { DialogModule } from 'primeng/dialog';
-import { InputTextModule } from 'primeng/inputtext';
-import { RippleModule } from 'primeng/ripple';
+import { PrimeNGConfig, SharedModule } from 'primeng/api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    CommonModule,
     BrowserAnimationsModule,
-    FormsModule,
     BrowserModule,
+    CoreModule,
+    SharedModule,
     AppRoutingModule,
-    ButtonModule,
-    DialogModule,
-    RippleModule,
-    InputTextModule,
-    DialogModule,
-    CheckboxModule,
   ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
