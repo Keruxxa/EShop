717f095c557de939a4146b727184bba47d91b54c
Pashinin Kirill <Kirill.Pashinin@icl-services.com>
2024-12-14 13:49:15 +0300

    Base basket realization

--- src/ClientApp/src/app/basket/basket/basket.component.ts
+++ src/ClientApp/src/app/basket/basket/basket.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-basket',
   standalone: true,
   templateUrl: './basket.component.html',
   styleUrl: './basket.component.scss',
+  imports: [RouterLink],
 })
 export class BasketComponent {}
