a7c3dc4d76450d134d8cafe5b04717056127e5e6
Pashinin Kirill <Kirill.Pashinin@icl-services.com>
2025-07-13 11:08:37 +0300

    feat(npm): update PrimeNG

--- src/ClientApp/src/app/app.module.ts
+++ src/ClientApp/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
-import { PrimeNGConfig, SharedModule } from 'primeng/api';
+import { SharedModule } from 'primeng/api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { CoreModule } from './core/core.module';
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { defaultPreset } from './theming/theme-preset';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,11 +23,22 @@ import { CoreModule } from './core/core.module';
     LayoutComponent,
     RouterOutlet,
   ],
-  providers: [provideClientHydration(), provideHttpClient(withFetch())],
+  providers: [
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: defaultPreset,
+        options: {
+          darkModeSelector: false || 'none',
+        },
+      },
+      ripple: true,
+    }),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
-  constructor(private primengConfig: PrimeNGConfig) {
-    this.primengConfig.ripple = true;
-  }
+  constructor() {}
 }
