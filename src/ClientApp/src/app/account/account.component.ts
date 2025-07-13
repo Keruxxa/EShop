e6cab318764caf0cb2d60d3e31af5f6b41031cc8
Pashinin Kirill <Kirill.Pashinin@icl-services.com>
2025-07-13 17:10:24 +0300

    account

--- src/ClientApp/src/app/account/account.component.ts
+++ src/ClientApp/src/app/account/account.component.ts
@@ -1,13 +1,69 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { RippleModule } from 'primeng/ripple';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { InputMaskModule } from 'primeng/inputmask';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TabsModule } from 'primeng/tabs';
+
+export interface AccountTab {
+  label: string;
+  route: string;
+  icon?: string;
+}
 
 @Component({
   standalone: true,
+  imports: [
+    RippleModule,
+    InputTextModule,
+    InputTextModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MenuModule,
+    ButtonModule,
+    InputMaskModule,
+    RouterModule,
+    CommonModule,
+    TabsModule,
+  ],
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AccountComponent {
+  protected formGoup: FormGroup;
+
+  protected tabs: AccountTab[];
+
   constructor() {
-    const temp = 0;
+    this.formGoup = new FormGroup({
+      firstName: new FormControl(null),
+      lastName: new FormControl(null),
+      email: new FormControl(null),
+      phone: new FormControl(null),
+    });
+
+    this.tabs = [
+      {
+        label: 'Персональные данные',
+        route: 'profile',
+        icon: 'pi pi-user',
+      },
+      {
+        label: 'Уведомления',
+        route: 'notifications',
+        icon: 'pi pi-bell',
+      },
+      {
+        label: 'Заказы',
+        route: 'orders',
+        icon: 'pi pi-shopping-cart',
+      },
+    ];
   }
 }
--- src/ClientApp/src/app/app-routing.module.ts
+++ src/ClientApp/src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { BasketComponent } from './basket/basket/basket.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { SignInComponent } from './features/sign-in/sign-in.component';
 import { SignUpComponent } from './features/sign-up/sign-up.component';
+import { ProfileComponent } from './account/profile/profile.component';
+import { NotificationsComponent } from './account/notifications/notifications.component';
+import { OrdersComponent } from './account/orders/orders.component';
 
 const routes: Routes = [
   {
@@ -12,7 +15,16 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [
       { path: 'basket', component: BasketComponent },
-      { path: 'account', component: AccountComponent },
+      {
+        path: 'account',
+        component: AccountComponent,
+        children: [
+          { path: 'profile', component: ProfileComponent },
+          { path: 'notifications', component: NotificationsComponent },
+          { path: 'orders', component: OrdersComponent },
+          { path: '', redirectTo: 'profile', pathMatch: 'full' },
+        ],
+      },
     ],
   },
   { path: 'sign-in', component: SignInComponent },
