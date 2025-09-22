4a6b220910af0a45f94a5abbc89b9078fff94aab
Pashinin Kirill <Kirill.Pashinin@icl-services.com>
2025-09-22 06:19:16 +0300

    feat(net-migration): migrate to .net9

---
+++ src/EShop.Infrastructure/Data/Migrations/20250922033139_Migrate_To_Net9.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShop.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migrate_To_Net9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Products");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Rating",
+                table: "Products",
+                type: "numeric",
+                nullable: true);
+        }
+    }
+}
